b3146385fdcecba83ff4430727d65c43a053453f
Minh Nguyen <fobissea@yahoo.com>
2022-09-21 13:33:19 +0900

    i deleted the comments I hope they will come back

--- hangman.py
+++ hangman.py
@@ -1,22 +1,3 @@
-# k=0
-# while True:
-# 	answer=int(input('Write down a number from 0 to 9. '))
-# 	print(answer)
-# 	k+=1
-# 	# answerAsList.append(answer)
-# 	# showAnswer.append("")
-# 	# if you want to end the process of making hangman answer:
-# 		StopCreating=int(input('Type "-1" to submit your Hangman answer. '))
-# 		if StopCreating==-1:
-# 			break
-# 	# else:
-# 		# go to line 19
-
-# TODO: ^
-# 		|  september 18
-################################################
-
-# answerAsList = [7 , 3 ,5 , 5 , 6 ,0 , 8 ]
 answer='agamemnon'
 answerAsList = ['a','g','a','m','e','m','n','o','n']
 
@@ -30,7 +11,6 @@ j=0
 gameOver=5 	#should be 10
 
 for w in range(22):
-	# if w<0: #first 3 guesses are normal
 	guess=input('Guess a letter or the word: ') ##'Guess a letter or submit "exit" if you want to close the game: '
 	if guess=="exit": 	#exit button
 		break
@@ -61,49 +41,4 @@ for w in range(22):
 			break
 		else:
 			print(f"Guess again. Number of guesses left: {gameOver-j}")	##
-	# else:	#after 3 guesses
-	# 	guess=input('Submit "Word" if you want to guess the whole word, or "letter" if you want to guess another letter: ')
-	# 	if guess=="Word" or guess=="word":
-	# 		guess=input('Guess the word or words: ')
-	# 		if guess==''.join(answerAsList):
-	# 			print(f"{' '.join(answerAsList)} Congratulations. You won!")
-	# 			break
-	# 		else:	#wrong guess made
-	# 			DoNotRepeat.append(','.join(guess))	#need to append all the letters guessed
-	# 												#stackOverflow showed me that trick
-	# 			j+=1
-	# 			if j==gameOver:
-	# 				print(f"Game over. You've made {gameOver} wrong guesses")
-	# 				break
-	# 			else:
-	# 				print(f"Guess again. Number of guesses left: {gameOver-j}")
-	# 	elif guess=="letter" or guess=="Letter":
-	# 		guess=input('Guess a letter or submit "exit" if you want to close the game: ') ##
-	# 		if guess=="exit": 	#exit button
-	# 			break
-	# 		if guess in answerAsList: 		#if right guess made
-	# 			print(guess, 'is correct') 	## you see this
-	# 			for i in range(len(answerAsList)):	#'cos we need to CHECK ALL SLOTS in the list
-	# 				if guess==answerAsList[i]: 		#finding right place for right guess in 1 list
-	# 					showAnswer[i]=guess 		#putting the guess to the same slot in a different, same-length list
-	# 			print(' '.join(showAnswer)) 	##joins list elements with space 
-	# 											##and replace empty elements with underscore
-	# 			if showAnswer==answerAsList:	#if all right guesses are made
-	# 											#aka. showAnswer has been filled up correctly
-	# 				print("Congratulations. You won!")
-	# 				break
-	# 		else: #if wrong guess made
-	# 			if guess in DoNotRepeat:	#if it's wrong and also same as a previous guess 
-	# 										#(i.e. is in DoNotRepeat)
-	# 				print("You've already made this guess. Make a different guess.")	##
-	# 			else:	#if it's wrong and not the same as a previous guess
-	# 				DoNotRepeat.append(guess) 	
-	# 			j+=1
-	# 			if j==gameOver:
-	# 				print(f"Game over. You've made {gameOver} wrong guesses")
-	# 				break
-	# 			else:
-	# 				print(f"Guess again. Number of guesses left: {gameOver-j}")	##
-##########################################################################################
-			# if j==2: # currently, 1 wrong guess in the "box"
-				# print("You've already made this guess. Please guess again with a different guess.")
+	

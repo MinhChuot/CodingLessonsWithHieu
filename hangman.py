be70f2f681b6395a19f5d35bff9029dfaac2f073
Minh Nguyen <fobissea@yahoo.com>
2022-09-23 00:27:09 +0900

    just wow

--- hangman.py
+++ hangman.py
@@ -1,40 +1,27 @@
 # k=0
 answerAsList = []
 showAnswer=[]
-while True:
-	answer=(input('Write down your hangman guessword: ')) #'Write down a number from 0 to 9. '
-	print(answer)
-	# k+=1
-	answerAsList.append(','.join(answer))
-	print(answerAsList)
-	for i in range(len(answerAsList)):
-		showAnswer.append("_")
-	print(showAnswer)
-	# if you want to end the process of making hangman answer:
-# 		StopCreating=int(input('Type "-1" to submit your Hangman answer. '))
-# 		if StopCreating==-1:
-# 			break
-# 	# else:
-# 		# go to line 19
-	print(f"Your hangman guessword is {' '.join(answerAsList)}. We may begin hangman")
-	break
-
-# TODO: ^
-# 		|  september 18
-################################################
-
-# answerAsList = [7 , 3 ,5 , 5 , 6 ,0 , 8 ]
-# answer='agamemnon'
-# answerAsList = ['a','g','a','m','e','m','n','o','n']
-
-# showAnswer=[]
-# for i in range(len(answerAsList)):
-# 	showAnswer.append("_")
 
 DoNotRepeat=[]
 
 j=0
 gameOver=5 	#should be 10
+while True:
+	answer=(input('Write down your hangman guessword: ')) #'Write down a number from 0 to 9. '
+	print(answer)
+	# k+=1
+	for letter in answer:
+		answerAsList.append(letter)	#got it from geeksforgeeks
+	print(answerAsList)
+	for i in range(len(answerAsList)):
+		showAnswer.append("_")
+	print(showAnswer)
+	print(f"Your hangman guessword is {''.join(answerAsList)}. We may begin hangman")
+	break
+
+# answerAsList = [7 , 3 ,5 , 5 , 6 ,0 , 8 ]
+# answer='agamemnon'
+# answerAsList = ['a','g','a','m','e','m','n','o','n']
 
 for w in range(22): #to Hieu: how do i do While True: correctly?
 	guess=input('Guess a letter or the word: ') ##'Guess a letter or submit "exit" if you want to close the game: '
@@ -59,7 +46,6 @@ for w in range(22): #to Hieu: how do i do While True: correctly?
 		if guess in DoNotRepeat:	#if it's wrong and also same as a previous guess 
 									#(i.e. is in DoNotRepeat)
 			print("You've already made this guess. Make a different guess.")
-			break
 		else:	#if it's wrong and not the same as a previous guess
 			DoNotRepeat.append(guess) 	
 			j+=1

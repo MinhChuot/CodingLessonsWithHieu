658cd3ebce2c5d8ed6c7b5e9af8fc6706c36faa2
Minh Nguyen <fobissea@yahoo.com>
2022-09-26 10:38:02 +0900

    fixed the TypeError stopped assigning numbers to checkedNum

--- sudoku/main.py
+++ sudoku/main.py
@@ -51,10 +51,11 @@ uncheckedNum=[1,2,3,4,5,6,7,8,9]
 
 for i in range(len(toAssess)):              # "for 9 times, and i goes 0 to 8"
   for j in range(len(toAssess[i])):         #i is 0. "for 9 times, j goes 0 to 8" 
-    if toAssess[i][j] in checkedNum:  #TypeError: argument of type 'int' is not iterable
+    if toAssess[i][j] in checkedNum:  
       print("2 of the same number is in this row")
     elif toAssess[i][j] in uncheckedNum:      #j is 0, 'if this number is anywhere 1-9'.  [0][0] is 5, so toAssess[i][j] is 5 for now. 
-      checkedNum=uncheckedNum.pop(toAssess[i][j]-1)   #if 5 (aka.toAssess[i][j]) is in uncheckedNum then it is at position unCheckedNum[5-1]   
+      checkedNum.append(toAssess[i][j])
+      uncheckedNum.pop(toAssess[i][j]-1)
 #     IndexError: pop index out of range
       print(checkedNum) # printed 5, from [0][0]
       print(uncheckedNum)

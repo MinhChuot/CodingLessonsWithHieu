73db941287d79e6628d2d6f0d8c064f1606ab5e7
Minh Nguyen <fobissea@yahoo.com>
2022-09-28 20:14:29 +0900

    Updates: finished the lines for the checking of columns I finished it in main, but the end product was very messy, so I moved most of the mess to Tests.py

--- sudoku/Tests.py
+++ sudoku/Tests.py
@@ -72,18 +72,12 @@
 # [1, 2, 4, 7, 8, 9]
 
 ######################################################################################
-shouldBeCorrect=[
-  [5, 3, 4, 6, 7, 8, 9, 1, 2],
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]
-toAssess=shouldBeCorrect
+
+# ***Checking columns***
+# column 1: j is 0; column 2: j is 1; column 3: j is 2;... column 9: j is 8
+# column 1: [0->8][0];  column 2: [0->8][1];... column 9: [0->8][8]
+
+#TODO: do columns next. An idea: switch columns into rows then check those new rows that way
 col1=[]
 col2=[]
 col3=[]
@@ -93,10 +87,10 @@ col6=[]
 col7=[]
 col8=[]
 col9=[]
-invertedNew=[]
-# invertedNew=[
+invertedShouldBeCorrect=[]
+# invertedShouldBeCorrect=[
 # [],[],[],[],[],[],[],[],[]
-# ]
+# ]     This is in case I need to append during the loop instead of having it as lines in the script 
 for i in range(9):
   for j in range(9):
     if j==0:
@@ -126,17 +120,16 @@ for i in range(9):
 # print(col7)
 # print(col8)
 # print(col9)
-invertedNew.append(col1)
-invertedNew.append(col2)
-invertedNew.append(col3)
-invertedNew.append(col4)
-invertedNew.append(col5)
-invertedNew.append(col6)
-invertedNew.append(col7)
-invertedNew.append(col8)
-invertedNew.append(col9)
-
-# print(invertedNew)
+invertedShouldBeCorrect.append(col1)
+invertedShouldBeCorrect.append(col2)
+invertedShouldBeCorrect.append(col3)
+invertedShouldBeCorrect.append(col4)
+invertedShouldBeCorrect.append(col5)
+invertedShouldBeCorrect.append(col6)
+invertedShouldBeCorrect.append(col7)
+invertedShouldBeCorrect.append(col8)
+invertedShouldBeCorrect.append(col9)
+# print(invertedShouldBeCorrect)
 # Output:
 # [
 # [5, 6, 1, 8, 4, 7, 9, 2, 3],
@@ -150,6 +143,40 @@ invertedNew.append(col9)
 # [2, 8, 7, 3, 1, 6, 4, 5, 9]
 # ]
 
+toAssess=invertedShouldBeCorrect
+# print(toAssess)
+# Output:
+# [
+# [5, 6, 1, 8, 4, 7, 9, 2, 3], 
+# [3, 7, 9, 5, 2, 1, 6, 8, 4], 
+# [4, 2, 8, 9, 6, 3, 1, 7, 5], 
+# [6, 1, 3, 7, 8, 9, 5, 4, 2], 
+# [7, 9, 4, 6, 5, 2, 3, 1, 8], 
+# [8, 5, 2, 1, 3, 4, 7, 9, 6], 
+# [9, 3, 5, 4, 7, 8, 2, 6, 1], 
+# [1, 4, 6, 2, 9, 5, 8, 3, 7], 
+# [2, 8, 7, 3, 1, 6, 4, 5, 9]
+# ]
+
+invertedNew=[]
+
+for row in range(9):  
+  for col in range(9):         #row is 0. 
+    if toAssess[row][col] in checkedNum:
+      print("2 of the same number is in this row")
+      break
+    elif toAssess[row][col] in noZero:   #if it's not a zero
+      checkedNum.append(toAssess[row][col])
+  # print(checkedNum) # printed 5, from [0][0]
+  invertedNew.append(checkedNum)
+  checkedNum=[]
+# print(invertedNew)
+
+if invertedNew==toAssess:
+  print("Columns are all valid")
+else:
+  print("There is/are one or more invalid column/s")
+
 # for i in range(9):
 #   for j in range(9):
 #     if j==0:
@@ -158,8 +185,7 @@ invertedNew.append(col9)
 #     elif j==1:
 #       col2.append(toAssess[i][j])
 #       print(col2)  *******************
-
-# Output:
+# Output:   Below might help with invertedShouldBeCorrect=[ [],[],[],[],[],[],[],[],[] ] 
 # [5]
 # [3]
 # [5, 6]
@@ -178,9 +204,3 @@ invertedNew.append(col9)
 # [3, 7, 9, 5, 2, 1, 6, 8]
 # [5, 6, 1, 8, 4, 7, 9, 2, 3]
 # [3, 7, 9, 5, 2, 1, 6, 8, 4]
-
-
-# column 1: j is 0; column 2: j is 1; column 3: j is 2;... column 9: j is 8
-# column 1: [0->8][0];  column 2: [0->8][1];... column 9: [0->8][8]
-
-#TODO: do columns next. An idea: switch columns into rows then check those new rows that way
--- sudoku/main.py
+++ sudoku/main.py
@@ -50,6 +50,7 @@ new=[]
 checkedNum=[]   #number we checked already can't appear again, so if it's checked any more times after the 1st, it's wrong
 noZero=[1,2,3,4,5,6,7,8,9]
 
+# ***Checking rows***
 for row in range(9):  
   for col in range(9):         #row is 0. 
     if toAssess[row][col] in checkedNum:
@@ -57,15 +58,16 @@ for row in range(9):
       break
     elif toAssess[row][col] in noZero:   #if it's not a zero
       checkedNum.append(toAssess[row][col])
-      print(checkedNum) # printed 5, from [0][0]
+      # print(checkedNum) # printed 5, from [0][0]
   new.append(checkedNum)
-  print(new)
+  # print(new)
   checkedNum=[]
 if new==toAssess:
-  print("yes")
+  print("Rows are all valid")
 else:
-  print("no")
+  print("There is/are one or more invalid row/s")
 
+# ***Checking columns***
 col1=[]
 col2=[]
 col3=[]
@@ -75,10 +77,10 @@ col6=[]
 col7=[]
 col8=[]
 col9=[]
-invertedNew=[]
-# invertedNew=[
+invertedShouldBeCorrect=[]
+# invertedShouldBeCorrect=[
 # [],[],[],[],[],[],[],[],[]
-# ]
+# ]     This is in case I need to append during the loop instead of having it as lines in the script 
 for i in range(9):
   for j in range(9):
     if j==0:
@@ -99,72 +101,36 @@ for i in range(9):
       col8.append(toAssess[i][j])
     elif j==8:
       col9.append(toAssess[i][j])
-# print(col1)
-# print(col2)
-# print(col3)
-# print(col4)
-# print(col5)
-# print(col6)
-# print(col7)
-# print(col8)
-# print(col9)
-invertedNew.append(col1)
-invertedNew.append(col2)
-invertedNew.append(col3)
-invertedNew.append(col4)
-invertedNew.append(col5)
-invertedNew.append(col6)
-invertedNew.append(col7)
-invertedNew.append(col8)
-invertedNew.append(col9)
+invertedShouldBeCorrect.append(col1)
+invertedShouldBeCorrect.append(col2)
+invertedShouldBeCorrect.append(col3)
+invertedShouldBeCorrect.append(col4)
+invertedShouldBeCorrect.append(col5)
+invertedShouldBeCorrect.append(col6)
+invertedShouldBeCorrect.append(col7)
+invertedShouldBeCorrect.append(col8)
+invertedShouldBeCorrect.append(col9)
 
+toAssess=invertedShouldBeCorrect
+invertedNew=[]
+
+for row in range(9):  
+  for col in range(9):         #row is 0. 
+    if toAssess[row][col] in checkedNum:
+      print("2 of the same number is in this row")
+      break
+    elif toAssess[row][col] in noZero:   #if it's not a zero
+      checkedNum.append(toAssess[row][col])
+  # print(checkedNum) # printed 5, from [0][0]
+  invertedNew.append(checkedNum)
+  checkedNum=[]
 # print(invertedNew)
-# Output:
-# [
-# [5, 6, 1, 8, 4, 7, 9, 2, 3],
-# [3, 7, 9, 5, 2, 1, 6, 8, 4],
-# [4, 2, 8, 9, 6, 3, 1, 7, 5], 
-# [6, 1, 3, 7, 8, 9, 5, 4, 2], 
-# [7, 9, 4, 6, 5, 2, 3, 1, 8], 
-# [8, 5, 2, 1, 3, 4, 7, 9, 6], 
-# [9, 3, 5, 4, 7, 8, 2, 6, 1], 
-# [1, 4, 6, 2, 9, 5, 8, 3, 7], 
-# [2, 8, 7, 3, 1, 6, 4, 5, 9]
-# ]
 
-# for i in range(9):
-#   for j in range(9):
-#     if j==0:
-#       col1.append(toAssess[i][j])
-#       print(col1) ********************
-#     elif j==1:
-#       col2.append(toAssess[i][j])
-#       print(col2)  *******************
+if invertedNew==toAssess:
+  print("Columns are all valid")
+else:
+  print("There is/are one or more invalid column/s")
 
-# Output:
-# [5]
-# [3]
-# [5, 6]
-# [3, 7]
-# [5, 6, 1]
-# [3, 7, 9]
-# [5, 6, 1, 8]
-# [3, 7, 9, 5]
-# [5, 6, 1, 8, 4]
-# [3, 7, 9, 5, 2]
-# [5, 6, 1, 8, 4, 7]
-# [3, 7, 9, 5, 2, 1]
-# [5, 6, 1, 8, 4, 7, 9]
-# [3, 7, 9, 5, 2, 1, 6]
-# [5, 6, 1, 8, 4, 7, 9, 2]
-# [3, 7, 9, 5, 2, 1, 6, 8]
-# [5, 6, 1, 8, 4, 7, 9, 2, 3]
-# [3, 7, 9, 5, 2, 1, 6, 8, 4]
-
-# column 1: j is 0; column 2: j is 1; column 3: j is 2;... column 9: j is 8
-# column 1: [0->8][0];  column 2: [0->8][1];... column 9: [0->8][8]
-
-#TODO: do columns next. An idea: switch columns into rows then check those new rows that way
 #TODO: for boxes, it may be similar to columns. Whereas with columns, you take 1 element of every list 9 times, with boxes, you take 3 elements for 3 times and repeat that 8 more times
 
 #   print(checkedNum)     # printed 3, from [0][1] or [3][8] or [8][0]?

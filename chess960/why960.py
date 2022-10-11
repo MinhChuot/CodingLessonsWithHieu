6e8db1a6ab32f6b794dc4c94584032e1496185c7
Minh Nguyen <fobissea@yahoo.com>
2022-10-11 13:11:03 +0900

    Start chess960 stuff

---
+++ chess960/why960.py
@@ -0,0 +1,12 @@
+# fischer random rules:
+	# there can be only 2 knights, 2 bishops, 2 rooks, 1 queen and 1 King and 8 pawns
+	# the white pawns must start at 2nd rank
+	# the black pawns must start at 7th rank
+	# the pieces must start at the back ranks (ranks 1 and 8)
+	# bishops must be on opposite-colored-squares
+
+# chess960 rules:
+	# same as fischer random, except that:
+	# there has to be a rook on either side of the king, i.e. both rooks cannot be on the same side of the king, 
+	# i.e. kings cannot be in the corners
+
